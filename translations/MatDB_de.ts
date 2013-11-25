a1755a74439fb5836bb3c6ac9316d44ca44097b0
Negusbuk <Andreas Mussgiller>
2013-11-25 15:24:30 +0100

    more german translations

--- translations/MatDB_de.ts
+++ translations/MatDB_de.ts
@@ -37,69 +37,69 @@
     <message>
         <location filename="../matdbmainwindow.cpp" line="76"/>
         <source>Import XML</source>
-        <translation type="unfinished"></translation>
+        <translation>XML Datei importieren</translation>
     </message>
     <message>
         <location filename="../matdbmainwindow.cpp" line="81"/>
         <source>Add Isotropic Material</source>
-        <translation type="unfinished"></translation>
+        <translation>isotropes Material hinzufügen</translation>
     </message>
     <message>
         <location filename="../matdbmainwindow.cpp" line="85"/>
         <source>Add Orthotropic Material</source>
-        <translation type="unfinished"></translation>
+        <translation>orthotropes Material hinzufügen</translation>
     </message>
     <message>
         <location filename="../matdbmainwindow.cpp" line="89"/>
         <source>Add Liquid Material</source>
-        <translation type="unfinished"></translation>
+        <translation>Flüssigkeit hinzufügen</translation>
     </message>
     <message>
         <location filename="../matdbmainwindow.cpp" line="93"/>
         <source>Add Gaseous Material</source>
-        <translation type="unfinished"></translation>
+        <translation>Gas hinzufügen</translation>
     </message>
     <message>
         <location filename="../matdbmainwindow.cpp" line="100"/>
         <location filename="../matdbmainwindow.cpp" line="418"/>
         <source>Hide Toolbox</source>
-        <translation type="unfinished"></translation>
+        <translation>Werkzeugkasten verstecken</translation>
     </message>
     <message>
         <location filename="../matdbmainwindow.cpp" line="106"/>
         <location filename="../matdbmainwindow.cpp" line="429"/>
         <source>Hide Categories</source>
-        <translation type="unfinished"></translation>
+        <translation>Kategorien verstecken</translation>
     </message>
     <message>
         <location filename="../matdbmainwindow.cpp" line="112"/>
         <source>Export XML</source>
-        <translation type="unfinished"></translation>
+        <translation>als XML Datei exportieren</translation>
     </message>
     <message>
         <location filename="../matdbmainwindow.cpp" line="113"/>
         <source>Export HTML</source>
-        <translation type="unfinished"></translation>
+        <translation>als HTML exportieren</translation>
     </message>
     <message>
         <location filename="../matdbmainwindow.cpp" line="139"/>
         <source>Property Collection</source>
-        <translation type="unfinished"></translation>
+        <translation>Eigenschaftenkatalog</translation>
     </message>
     <message>
         <location filename="../matdbmainwindow.cpp" line="148"/>
         <source>Categories</source>
-        <translation type="unfinished">Kategorien</translation>
+        <translation>Kategorien</translation>
     </message>
     <message>
         <location filename="../matdbmainwindow.cpp" line="156"/>
         <source>Properties</source>
-        <translation type="unfinished"></translation>
+        <translation>Eigenschaften</translation>
     </message>
     <message>
         <location filename="../matdbmainwindow.cpp" line="169"/>
         <source>Parameter</source>
-        <translation type="unfinished"></translation>
+        <translation>Parameter</translation>
     </message>
     <message>
         <location filename="../matdbmainwindow.cpp" line="256"/>
@@ -107,23 +107,23 @@
         <location filename="../matdbmainwindow.cpp" line="302"/>
         <location filename="../matdbmainwindow.cpp" line="307"/>
         <source>Export Materials</source>
-        <translation type="unfinished"></translation>
+        <translation>Materialien exportieren</translation>
     </message>
     <message>
         <location filename="../matdbmainwindow.cpp" line="337"/>
         <location filename="../matdbmainwindow.cpp" line="343"/>
         <source>Import Materials</source>
-        <translation type="unfinished"></translation>
+        <translation>Materialien importieren</translation>
     </message>
     <message>
         <location filename="../matdbmainwindow.cpp" line="421"/>
         <source>Show Toolbox</source>
-        <translation type="unfinished"></translation>
+        <translation>Werkzeugkasten zeigen</translation>
     </message>
     <message>
         <location filename="../matdbmainwindow.cpp" line="432"/>
         <source>Show Categories</source>
-        <translation type="unfinished"></translation>
+        <translation>Kategorien zeigen</translation>
     </message>
 </context>
 <context>
@@ -131,17 +131,17 @@
     <message>
         <location filename="../materialcategoryeditdialog.cpp" line="42"/>
         <source>Name:</source>
-        <translation type="unfinished"></translation>
+        <translation>Name:</translation>
     </message>
     <message>
         <location filename="../materialcategoryeditdialog.cpp" line="54"/>
         <source>Ok</source>
-        <translation type="unfinished"></translation>
+        <translation>Ok</translation>
     </message>
     <message>
         <location filename="../materialcategoryeditdialog.cpp" line="59"/>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation>Abbruch</translation>
     </message>
 </context>
 <context>
@@ -149,17 +149,17 @@
     <message>
         <location filename="../materialcategorymodel.cpp" line="34"/>
         <source>No Category</source>
-        <translation type="unfinished"></translation>
+        <translation>keine Kategorie</translation>
     </message>
     <message>
         <location filename="../materialcategorymodel.cpp" line="35"/>
         <source>Structural</source>
-        <translation type="unfinished"></translation>
+        <translation>Struktur</translation>
     </message>
     <message>
         <location filename="../materialcategorymodel.cpp" line="36"/>
         <source>Glue</source>
-        <translation type="unfinished"></translation>
+        <translation>Kleber</translation>
     </message>
 </context>
 <context>
@@ -167,12 +167,12 @@
     <message>
         <location filename="../materialfilterwidget.cpp" line="59"/>
         <source>AND</source>
-        <translation type="unfinished"></translation>
+        <translation>UND</translation>
     </message>
     <message>
         <location filename="../materialfilterwidget.cpp" line="63"/>
         <source>OR</source>
-        <translation type="unfinished"></translation>
+        <translation>ODER</translation>
     </message>
 </context>
 <context>
@@ -180,22 +180,22 @@
     <message>
         <location filename="../materialimportdialog.cpp" line="41"/>
         <source>Material</source>
-        <translation type="unfinished"></translation>
+        <translation>Material</translation>
     </message>
     <message>
         <location filename="../materialimportdialog.cpp" line="43"/>
         <source>Description</source>
-        <translation type="unfinished"></translation>
+        <translation>Beschreibung</translation>
     </message>
     <message>
         <location filename="../materialimportdialog.cpp" line="86"/>
         <source>Import</source>
-        <translation type="unfinished"></translation>
+        <translation>importieren</translation>
     </message>
     <message>
         <location filename="../materialimportdialog.cpp" line="93"/>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation>Abbruch</translation>
     </message>
 </context>
 <context>
@@ -203,22 +203,22 @@
     <message>
         <location filename="../materialmetadatawidget.cpp" line="49"/>
         <source>Description:</source>
-        <translation type="unfinished"></translation>
+        <translation>Beschreibung:</translation>
     </message>
     <message>
         <location filename="../materialmetadatawidget.cpp" line="56"/>
         <source>Category:</source>
-        <translation type="unfinished"></translation>
+        <translation>Kategorie:</translation>
     </message>
     <message>
         <location filename="../materialmetadatawidget.cpp" line="62"/>
         <source>Tags:</source>
-        <translation type="unfinished"></translation>
+        <translation>Etiketten:</translation>
     </message>
     <message>
         <location filename="../materialmetadatawidget.cpp" line="66"/>
         <source>Notes:</source>
-        <translation type="unfinished"></translation>
+        <translation>Notizen:</translation>
     </message>
 </context>
 <context>
@@ -226,28 +226,28 @@
     <message>
         <location filename="../materialparameterview.cpp" line="124"/>
         <source>Import</source>
-        <translation type="unfinished"></translation>
+        <translation>importieren</translation>
     </message>
     <message>
         <location filename="../materialparameterview.cpp" line="126"/>
         <source>Delete</source>
-        <translation type="unfinished"></translation>
+        <translation>löschen</translation>
     </message>
     <message>
         <location filename="../materialparameterview.cpp" line="127"/>
         <source>Delete Temperature</source>
-        <translation type="unfinished"></translation>
+        <translation>Temperatur löschen</translation>
     </message>
     <message>
         <location filename="../materialparameterview.cpp" line="146"/>
         <source>Temperature</source>
-        <translation type="unfinished"></translation>
+        <translation>Temperatur</translation>
     </message>
     <message>
         <location filename="../materialparameterview.cpp" line="451"/>
         <location filename="../materialparameterview.cpp" line="456"/>
         <source>Import Data</source>
-        <translation type="unfinished"></translation>
+        <translation>Daten importieren</translation>
     </message>
 </context>
 <context>
@@ -255,22 +255,22 @@
     <message>
         <location filename="../materialpropertyview.cpp" line="157"/>
         <source>Delete</source>
-        <translation type="unfinished"></translation>
+        <translation>löschen</translation>
     </message>
     <message>
         <location filename="../materialpropertyview.cpp" line="165"/>
         <source>Property</source>
-        <translation type="unfinished"></translation>
+        <translation>Eigenschaft</translation>
     </message>
     <message>
         <location filename="../materialpropertyview.cpp" line="166"/>
         <source>Value</source>
-        <translation type="unfinished"></translation>
+        <translation>Wert</translation>
     </message>
     <message>
         <location filename="../materialpropertyview.cpp" line="167"/>
         <source>Unit</source>
-        <translation type="unfinished"></translation>
+        <translation>Einheit</translation>
     </message>
 </context>
 <context>
@@ -278,17 +278,17 @@
     <message>
         <location filename="../materialselectiondialog.cpp" line="45"/>
         <source>Material</source>
-        <translation type="unfinished"></translation>
+        <translation>Material</translation>
     </message>
     <message>
         <location filename="../materialselectiondialog.cpp" line="47"/>
         <source>Description</source>
-        <translation type="unfinished"></translation>
+        <translation>Beschreibung</translation>
     </message>
     <message>
         <location filename="../materialselectiondialog.cpp" line="98"/>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation>Abbruch</translation>
     </message>
 </context>
 <context>
@@ -296,17 +296,17 @@
     <message>
         <location filename="../materialtableview.cpp" line="62"/>
         <source>Material</source>
-        <translation type="unfinished"></translation>
+        <translation>Material</translation>
     </message>
     <message>
         <location filename="../materialtableview.cpp" line="79"/>
         <source>Delete</source>
-        <translation type="unfinished"></translation>
+        <translation>löschen</translation>
     </message>
     <message>
         <location filename="../materialtableview.cpp" line="81"/>
         <source>Duplicate</source>
-        <translation type="unfinished"></translation>
+        <translation>duplizieren</translation>
     </message>
 </context>
 <context>
@@ -314,27 +314,27 @@
     <message>
         <location filename="../materialxmlexportdialog.cpp" line="44"/>
         <source>Material</source>
-        <translation type="unfinished"></translation>
+        <translation>Material</translation>
     </message>
     <message>
         <location filename="../materialxmlexportdialog.cpp" line="46"/>
         <source>Description</source>
-        <translation type="unfinished"></translation>
+        <translation>Beschreibung</translation>
     </message>
     <message>
         <location filename="../materialxmlexportdialog.cpp" line="90"/>
         <source>Export Mode:</source>
-        <translation type="unfinished"></translation>
+        <translation>Modus:</translation>
     </message>
     <message>
         <location filename="../materialxmlexportdialog.cpp" line="115"/>
         <source>Export</source>
-        <translation type="unfinished"></translation>
+        <translation>exportieren</translation>
     </message>
     <message>
         <location filename="../materialxmlexportdialog.cpp" line="122"/>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation>Abbruch</translation>
     </message>
 </context>
 <context>
@@ -343,12 +343,12 @@
         <location filename="../materialpropertyview.cpp" line="78"/>
         <location filename="../materialpropertyview.cpp" line="100"/>
         <source>Undefined</source>
-        <translation type="unfinished"></translation>
+        <translation>undefiniert</translation>
     </message>
     <message>
         <location filename="../materialpropertyview.cpp" line="106"/>
         <source>Table</source>
-        <translation type="unfinished"></translation>
+        <translation>Tabelle</translation>
     </message>
 </context>
 </TS>
